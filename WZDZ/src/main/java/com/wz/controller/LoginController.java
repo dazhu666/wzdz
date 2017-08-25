aeb80ad50365eeae1639bfea49d31b5b4d3f61eb
梁大柱 <liangdz@teleware.cn>
2017-08-25 10:57:08 +0800

    新增日子输出配置

---
+++ WZDZ/src/main/java/com/wz/controller/LoginController.java
@@ -0,0 +1,18 @@
+package com.wz.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by Administrator on 2017/8/25.
+ */
+@RestController
+@RequestMapping("/login")
+public class LoginController {
+    @RequestMapping("/login")
+    public ResponseEntity<?> login(){
+        return new ResponseEntity<Object>("登录", HttpStatus.OK);
+    }
+}
--- WZDZ/src/main/java/com/wz/mapper/UserMapper.java
+++ WZDZ/src/main/java/com/wz/mapper/UserMapper.java
@@ -2,9 +2,11 @@ package com.wz.mapper;
 
 import com.baomidou.mybatisplus.mapper.BaseMapper;
 import com.wz.domain.entity.User;
+import org.apache.ibatis.annotations.Mapper;
 
 /**
  * Created by Administrator on 2017/8/24.
  */
+@Mapper
 public interface UserMapper extends BaseMapper<User> {
 }
